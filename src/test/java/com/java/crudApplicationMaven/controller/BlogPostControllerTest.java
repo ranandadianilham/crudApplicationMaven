6c67190ebc72851beffd0bb516e4cea186cc1ffc
Rananda Dian Ilham <nanda@eluon.com>
2023-05-22 20:58:22 +0700

    add test

--- src/test/java/com/java/crudApplicationMaven/controller/BlogPostControllerTest.java
+++ src/test/java/com/java/crudApplicationMaven/controller/BlogPostControllerTest.java
@@ -37,146 +37,165 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest // Load the Spring application context
 class BlogPostControllerTest {
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+        @Autowired
+        private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Autowired
+        private ObjectMapper objectMapper;
 
-    private MockMvc mockMvc;
+        private MockMvc mockMvc;
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
+        @BeforeEach
+        public void setup() {
+                mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        }
 
-    private static final String SECRET_KEY = "6150645367566B5970337336763979244226452948404D6251655468576D5A71";
+        private static final String SECRET_KEY = "6150645367566B5970337336763979244226452948404D6251655468576D5A71";
 
-    @Test
-    public void testGetAllPosts() throws Exception {
-        // Prepare the request body
-        PostGetAllRequest request = new PostGetAllRequest();
-        request.setSortType("ASC");
-        request.setPageNo(0);
-        request.setRowPerPage(10);
-        request.setSortBy("title");
+        @Test
+        public void testGetAllPosts() throws Exception {
+                // Prepare the request body
+                PostGetAllRequest request = new PostGetAllRequest();
+                request.setSortType("ASC");
+                request.setSortBy("title");
 
-        // Perform the request
-        ResultActions resultActions = mockMvc.perform(post("/blog/post/list")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)));
+                // Perform the request
+                ResultActions resultActions = mockMvc.perform(post("/blog/post/list/all")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request)));
 
-        // Verify the response
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
-                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
-                .andExpect(jsonPath("$.data").exists());
-    }
+                // Verify the response
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
+                                .andExpect(jsonPath("$.data").exists());
+        }
 
-    @Test
-    public void testGetPostById() throws Exception {
-        // Prepare the request path variable
-        Long postId = 1L;
+        @Test
+        public void testGetAllPostsPaginated() throws Exception {
+                // Prepare the request body
+                PostGetAllRequest request = new PostGetAllRequest();
+                request.setSortType("ASC");
+                request.setPageNo(0);
+                request.setRowPerPage(10);
+                request.setSortBy("title");
 
-        // Perform the request
-        ResultActions resultActions = mockMvc.perform(get("/blog/post/{id}", postId));
+                // Perform the request
+                ResultActions resultActions = mockMvc.perform(post("/blog/post/list/paginated")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request)));
 
-        // Verify the response
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
-                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
-                .andExpect(jsonPath("$.data").exists());
-    }
+                // Verify the response
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
+                                .andExpect(jsonPath("$.data").exists());
+        }
 
-    @Test
-    public void testAddPost() throws Exception {
-        // Prepare the request body
-        Post postCase = new Post();
-        postCase.setId(1L);
-        postCase.setTitle("New Post");
-        postCase.setAuthor("John Doe");
-        postCase.setBody("Lorem ipsum");
+        @Test
+        public void testGetPostById() throws Exception {
+                // Prepare the request path variable
+                Long postId = 1L;
 
-        String jwtToken = generateJwtToken();
+                // Perform the request
+                ResultActions resultActions = mockMvc.perform(get("/blog/post/{id}", postId));
 
-        // Perform the request
-        ResultActions resultActions = mockMvc.perform(post("/blog/post/add")
-                .header("Authorization", "Bearer " + jwtToken)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(postCase)));
+                // Verify the response
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
+                                .andExpect(jsonPath("$.data").exists());
+        }
 
-        // Verify the response
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
-                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
-                .andExpect(jsonPath("$.data").exists());
-    }
+        @Test
+        public void testAddPost() throws Exception {
+                // Prepare the request body
+                Post postCase = new Post();
+                postCase.setId(1L);
+                postCase.setTitle("New Post");
+                postCase.setAuthor("John Doe");
+                postCase.setBody("Lorem ipsum");
 
-    @Test
-    public void testUpdatePostById() throws Exception {
-        Post postCase = new Post();
-        postCase.setId(1L);
-        postCase.setTitle("New Post");
-        postCase.setAuthor("John Doe");
-        postCase.setBody("Lorem ipsum");
+                String jwtToken = generateJwtToken();
 
-        String jwtToken = generateJwtToken();
+                // Perform the request
+                ResultActions resultActions = mockMvc.perform(post("/blog/post/add")
+                                .header("Authorization", "Bearer " + jwtToken)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(postCase)));
 
-        // Perform the request
-        ResultActions resultActions = mockMvc.perform(post("/blog/post/update/" + 1)
-                .header("Authorization", "Bearer " + jwtToken)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(postCase)));
+                // Verify the response
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
+                                .andExpect(jsonPath("$.data").exists());
+        }
 
-        // Verify the response
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
-                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
-                .andExpect(jsonPath("$.data").exists());
-    }
+        @Test
+        public void testUpdatePostById() throws Exception {
+                Post postCase = new Post();
+                postCase.setId(1L);
+                postCase.setTitle("New Post");
+                postCase.setAuthor("John Doe");
+                postCase.setBody("Lorem ipsum");
 
-    @Test
-    public void testDeletePostById() throws Exception {
-        String jwtToken = generateJwtToken();
-        ResultActions resultActions = mockMvc.perform(post("/blog/post/delete/" + 1)
-                .header("Authorization", "Bearer " + jwtToken)
-                .contentType(MediaType.APPLICATION_JSON));
+                String jwtToken = generateJwtToken();
 
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
-                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()));
-    }
+                // Perform the request
+                ResultActions resultActions = mockMvc.perform(post("/blog/post/update/" + 1)
+                                .header("Authorization", "Bearer " + jwtToken)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(postCase)));
 
-    private static String generateJwtToken() {
-        // String secretKey =
-        // "6150645367566B5970337336763979244226452948404D6251655468576D5A71"; //
-        // Replace with your
-        // secret key
+                // Verify the response
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()))
+                                .andExpect(jsonPath("$.data").exists());
+        }
 
-        // Define token claims
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("username", "testuser");
-        claims.put("role", "user");
+        @Test
+        public void testDeletePostById() throws Exception {
+                String jwtToken = generateJwtToken();
+                ResultActions resultActions = mockMvc.perform(post("/blog/post/delete/" + 1)
+                                .header("Authorization", "Bearer " + jwtToken)
+                                .contentType(MediaType.APPLICATION_JSON));
 
-        // Set token expiration time
-        long currentTimeMillis = System.currentTimeMillis();
-        long expirationTimeMillis = currentTimeMillis + (60 * 60 * 1000); // 1 hour
-        Date expirationDate = new Date(expirationTimeMillis);
+                resultActions.andExpect(status().isOk())
+                                .andExpect(jsonPath("$.code").value(ResponseStatusCode.SUCCESS.code()))
+                                .andExpect(jsonPath("$.desc").value(ResponseStatusCode.SUCCESS.desc()));
+        }
 
-        // Generate the token
-        String token = Jwts.builder()
-                .setClaims(claims)
-                .setExpiration(expirationDate)
-                /* .signWith(SignatureAlgorithm.HS256, secretKey) */
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
+        private static String generateJwtToken() {
+                // String secretKey =
+                // "6150645367566B5970337336763979244226452948404D6251655468576D5A71"; //
+                // Replace with your
+                // secret key
 
-        return token;
-    }
+                // Define token claims
+                Map<String, Object> claims = new HashMap<>();
+                claims.put("username", "testuser");
+                claims.put("role", "user");
 
-    private static Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+                // Set token expiration time
+                long currentTimeMillis = System.currentTimeMillis();
+                long expirationTimeMillis = currentTimeMillis + (60 * 60 * 1000); // 1 hour
+                Date expirationDate = new Date(expirationTimeMillis);
+
+                // Generate the token
+                String token = Jwts.builder()
+                                .setClaims(claims)
+                                .setExpiration(expirationDate)
+                                /* .signWith(SignatureAlgorithm.HS256, secretKey) */
+                                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                                .compact();
+
+                return token;
+        }
+
+        private static Key getSignInKey() {
+                byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+                return Keys.hmacShaKeyFor(keyBytes);
+        }
 
 }
